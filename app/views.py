ac74d27674883307b791801be8ef3aad3e0b537a
Kate Heddleston <kate@heddleston.com>
2014-06-22 19:40:45 -0700

    Add a about page.

--- app/views.py
+++ app/views.py
@@ -1,9 +1,19 @@
 from app import app
 from app.utils.decorators.template_globals import use_template_globals
+
+from flask import g
 from flask import render_template
 
 
 @app.route('/')
 @use_template_globals
 def index():
+    g.nav_view = 'home'
     return render_template('home.html')
+
+
+@app.route('/about')
+@use_template_globals
+def about():
+    g.nav_view = 'about'
+    return render_template('about.html')
