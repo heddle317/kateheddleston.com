a3726289c6b87061f3932545d6093ab1884cfe97
Kate Heddleston <kate.heddleston@gmail.com>
2014-10-04 15:36:45 -0400

    Add link and created_at for tweets.

--- app/twitter.py
+++ app/twitter.py
@@ -2,6 +2,7 @@ import json
 import tweepy
 
 from app import config
+from app.utils.datetime_tools import format_date
 
 
 def get_tweet_comments(url):
@@ -25,9 +26,13 @@ def process_tweet(tweet):
             tweet_dict[field] = getattr(tweet, field)
     tweet_dict['user'] = json.dumps(tweet.user._json)
     tweet_dict['author'] = json.dumps(tweet.author._json)
-    tweet_dict['retweets'] = [json.dumps(retweet._json) for retweet in tweet.retweets()]
+    tweet_dict['created_at'] = format_date(tweet.created_at, '%B %d, %Y')
+    link = "https://www.twitter.com/{}/status/{}".format(tweet.author._json.get('screen_name'), tweet_dict.get('id'))
+    tweet_dict['link'] = link
+    tweet_dict.pop('retweets')
     tweet_dict.pop('favorite')
     tweet_dict.pop('retweet')
     tweet_dict.pop('parse')
     tweet_dict.pop('destroy')
+    tweet_dict.pop('place')
     return tweet_dict
