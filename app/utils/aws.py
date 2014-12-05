92efbd5e02c45341963cce78f2af21e9ebba5e7a
Kate Heddleston <kate@heddleston.com>
2014-12-05 19:26:33 +0700

    Create code to create multiple versions of photos with different image resolutions.

--- app/utils/aws.py
+++ app/utils/aws.py
@@ -1,9 +1,80 @@
-import boto
+import cStringIO
 import os
+import urllib
 
+from app import config
+
+from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
+from PIL import Image
 
-def connect_s3():
-    s3 = boto.connect_s3()
-    bucket_key = Key(os.environ.get('AWS_BUCKET_KEY'))
+
+def s3_change_image_resolutions(gallery_uuid, filename):
+    conn = S3Connection(config.AWS_ACCESS_KEY_ID, config.AWS_SECRET_ACCESS_KEY)
+    bucket = conn.get_bucket(config.AWS_IMAGE_BUCKET_KEY)
+    key = Key(bucket)
+
+    url = "{}/galleries/{}/{}".format(config.IMAGES_BASE, gallery_uuid, filename)
+    print url
+    # Retrieve our source image from a URL
+    fp = urllib.urlopen(url)
+    print fp
+    content_type = fp.info().get('content-type')
+    print content_type
+    # Load the URL data into an image
+    img = cStringIO.StringIO(fp.read())
+    img_original = Image.open(img)
+    print img_original
+
+    change_image_resolution(gallery_uuid, filename, img_original, content_type, .05, key)
+    change_image_resolution(gallery_uuid, filename, img_original, content_type, .10, key)
+    change_image_resolution(gallery_uuid, filename, img_original, content_type, .25, key)
+    change_image_resolution(gallery_uuid, filename, img_original, content_type, .50, key)
+    change_image_resolution(gallery_uuid, filename, img_original, content_type, .75, key)
+
+
+def change_image_resolution(gallery_uuid, filename, img_original, content_type, size_percent, key):
+    # Resize the image
+    size = (int(img_original.size[0] * size_percent), int(img_original.size[1] * size_percent))
+    print img_original.size
+    print size
+    img_resized = img_original.resize(size, Image.NEAREST)
+
+    # NOTE, we're saving the image into a cStringIO object to avoid writing to disk
+    out_location = cStringIO.StringIO()
+    file_type = get_file_type(content_type)
+    print file_type
+    img_resized.save(out_location, file_type)
+
+    key.key = 'galleries/{}/{}'.format(gallery_uuid, new_filename(filename, size))
+    print key.key
+    key.set_contents_from_string(out_location.getvalue(),
+                                 headers={'Content-Type': content_type},
+                                 replace=False,
+                                 policy='public-read')
+    out_location.close()
+
+
+def get_file_type(content_type):
+    c, ext = content_type.split('/')
+    return ext
+
+
+def new_filename(filename, size):
+    name, ext = os.path.splitext(filename)
+    width = size[0]
+    if width < 500:
+        additional_name = 'thumbnail'
+    elif width < 1000:
+        additional_name = 'small'
+    elif width < 2000:
+        additional_name = 'medium'
+    elif width < 4000:
+        additional_name = 'large'
+    elif width < 6000:
+        additional_name = 'grande'
+    else:
+        additional_name = 'jumbo'
+    print name, additional_name
+    return "{}_{}".format(name, additional_name)
---
+++ change_image_resolution.py
@@ -0,0 +1,16 @@
+import sys
+
+from app.db.galleries import Gallery
+from app.utils.aws import s3_change_image_resolutions
+
+
+if __name__ == '__main__':
+    gallery_uuid = sys.argv[1]
+    if len(sys.argv) > 2:
+        filename = sys.argv[2]
+        s3_change_image_resolutions(gallery_uuid, filename)
+    else:
+        gallery = Gallery.get_gallery(uuid=gallery_uuid)
+        for item in gallery.get('items', []):
+            print item.get('image_name')
+            s3_change_image_resolutions(gallery_uuid, item.get('image_name'))
