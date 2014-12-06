4c5140b6a3456ae3343a19245c5bc962b6dae703
Kate Heddleston <kate@heddleston.com>
2014-12-06 16:06:21 +0700

    Photo sizes now fit fixed widths.

--- app/utils/aws.py
+++ app/utils/aws.py
@@ -23,28 +23,27 @@ def s3_change_image_resolutions(gallery_uuid, filename):
     img = cStringIO.StringIO(fp.read())
     img_original = Image.open(img)
 
-    change_image_resolution(gallery_uuid, filename, img_original, content_type, .05, key)
-    change_image_resolution(gallery_uuid, filename, img_original, content_type, .10, key)
-    change_image_resolution(gallery_uuid, filename, img_original, content_type, .25, key)
-    change_image_resolution(gallery_uuid, filename, img_original, content_type, .50, key)
-    change_image_resolution(gallery_uuid, filename, img_original, content_type, .75, key)
-    change_image_resolution(gallery_uuid, filename, img_original, content_type, 1, key)
+    change_image_resolution(gallery_uuid, filename, img_original, content_type, 500, key)
+    change_image_resolution(gallery_uuid, filename, img_original, content_type, 1000, key)
+    change_image_resolution(gallery_uuid, filename, img_original, content_type, 2000, key)
+    change_image_resolution(gallery_uuid, filename, img_original, content_type, 4000, key)
+    change_image_resolution(gallery_uuid, filename, img_original, content_type, 6000, key)
 
 
-def change_image_resolution(gallery_uuid, filename, img_original, content_type, size_percent, key):
+def change_image_resolution(gallery_uuid, filename, img_original, content_type, width, key):
     # Resize the image
-    size = (int(img_original.size[0] * size_percent), int(img_original.size[1] * size_percent))
-    img_resized = img_original.resize(size, Image.NEAREST)
+    new_size = get_width_height(img_original.size, width)
+    img_resized = img_original.resize(new_size, Image.NEAREST)
 
     # NOTE, we're saving the image into a cStringIO object to avoid writing to disk
     out_location = cStringIO.StringIO()
     file_type = get_file_type(content_type)
     img_resized.save(out_location, file_type)
 
-    key.key = 'galleries/{}/{}'.format(gallery_uuid, new_filename(filename, size))
+    key.key = 'galleries/{}/{}'.format(gallery_uuid, new_filename(filename, new_size))
     key.set_contents_from_string(out_location.getvalue(),
                                  headers={'Content-Type': content_type},
-                                 replace=False,
+                                 replace=True,
                                  policy='public-read')
     out_location.close()
 
@@ -52,6 +51,14 @@ def change_image_resolution(gallery_uuid, filename, img_original, content_type, 
 def get_file_type(content_type):
     c, ext = content_type.split('/')
     return ext
+
+
+def get_width_height(original_size, width):
+    if width >= original_size[0]:
+        return original_size
+    percent = float(width) / float(original_size[0])
+    height = int(original_size[1] * percent)
+    return (width, height)
 
 
 def new_filename(filename, size):
