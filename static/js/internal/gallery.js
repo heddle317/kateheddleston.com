75ed2a08120b08f0ce2ba660bc2a39e2fd92248f
Kate Heddleston <kate.heddleston@gmail.com>
2015-02-27 15:33:41 +0700

    Fix issue with next and prev links.

--- static/js/internal/gallery.js
+++ static/js/internal/gallery.js
@@ -1,19 +1,11 @@
 angularApp.controller('PostController', ['$scope', '$http', '$log', function($scope, $http, $log) {
   $scope.position = 1;
-  $scope.nextLink = null;
-  $scope.prevLink = null;
   $http.get('/gallery/' + galleryUUID).success(function(response) {
       $scope.init(response);
   });
   $scope.init = function(gallery) {
     gallery = angular.fromJson(gallery);
     $scope.gallery = gallery;
-    if ($scope.gallery.next_uuid) {
-        $scope.nextLink = '/blog/' + $scope.gallery.next_uuid;
-    }
-    if ($scope.gallery.prev_uuid) {
-        $scope.prevLink = '/blog/' + $scope.gallery.prev_uuid;
-    }
     $scope.titles = [];
     for (var i = 0; i < $scope.gallery.items.length; i++) {
       if ($scope.gallery.items[i].title) {
