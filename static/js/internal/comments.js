e9672c9897015f46ae936d3ae6b700761a8040d6
Kate Heddleston <kate.heddleston@gmail.com>
2015-03-11 15:37:00 +0700

    Remove tooltip next to tweeting.

--- static/js/internal/comments.js
+++ static/js/internal/comments.js
@@ -1,10 +1,4 @@
-$(document).ready(function() {
-  $('#comment-tooltip').popover({html: true,
-                                 viewport: {'selector': 'body', 'padding': 20}});
-});
-
 angularApp.controller('CommentController', ['$scope', '$http', '$window', '$log', function($scope, $http, $window, $log) {
-  $scope.tooltipContent = "I use Twitter for comments because Twitter is largely a public forum. I don't want comment threads on my website to be a place where people can hide behind anonymity and make cruel remarks. I appreciate all thoughtful, funny, and constructive opinions about the content posted here. If you are not comfortable using Twitter to voice your thoughts then you may reach out to me directly.<br><br><strong>Why do I add the blog post link to the tweet?</strong><br>To shamelessly promote my content of course. Actually, the link is how I find the tweets through Twitter's API to display here."
   $scope.numComments = null;
   $scope.comments = [];
   $scope.rows = function() {
