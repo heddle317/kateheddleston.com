1de42c53bc045de4245b448e1cd266eb0c651c92
Kate Heddleston <kate.heddleston@gmail.com>
2015-03-17 16:29:09 +0700

    Add status section for subscriptions.

--- static/js/internal/subscribers.js
+++ static/js/internal/subscribers.js
@@ -3,6 +3,20 @@ angularApp.controller('SubscriptionController', ['$scope', '$http', '$window', '
     $scope.currentSubscriber = null;
     $http.get('/admin/api/subscribers').success(function(response) {
         $scope.subscribers = response;
+        $scope.activeSubscribers = [];
+        $scope.unverifiedSubscribers = [];
+        $scope.unsubscribed = [];
+        var s;
+        for (var i = 0; i < $scope.subscribers.length; i++) {
+            s = $scope.subscribers[i];
+            if (s.dead) {
+                $scope.unsubscribed.push(s);
+            } else if(s.verified) {
+                $scope.activeSubscribers.push(s);
+            } else {
+                $scope.unverifiedSubscribers.push(s);
+            }
+        }
     });
     $scope.sendVerificationEmail = function(subscriber) {
         var confirm = $window.confirm("Are you sure you want to resend " + subscriber.email +"'s verification email?");
